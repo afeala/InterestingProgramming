af931a4a21c3f08254fd619cc4e1c8bb2109353f
杨景辉 <jh.yang@csmall.com>
2015-09-07 15:15:48 +0800

    趣味编程练习

---
+++ study/src/_008BlackFriday.java
@@ -0,0 +1,37 @@
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Scanner;
+
+/**
+ * 项目名称：InterestingProgramming
+ * 项目包名：PACKAGE_NAME
+ * 类    名：_008BlackFriday
+ * 创 建 人：Rocky
+ * 创 建 于：2015/9/7 14:47
+ */
+public class _008BlackFriday {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("请输入起始年份：");
+        int year = input.nextInt();
+        System.out.print("请输入要打印未来几年的黑色星期五：");
+        int n = input.nextInt();
+        getBlackFriday(year,n);
+    }
+
+    public static void getBlackFriday(int year,int n){
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd E");    //设置日期输出格式
+        int k=0;
+        Calendar cal = Calendar.getInstance();  //获取日历对象
+        while (k<n){    //外循环控制年份
+            for (int i=0;i<12;i++){     //内循环控制月份
+                cal.set(year,i,13);     //设置日期
+                if (5==(cal.get(Calendar.DAY_OF_WEEK)-1)){      //判断是否是星期五
+                    System.out.println("黑色星期五："+sdf.format(cal.getTime()));     //打印输出黑色星期五
+                }
+            }
+            year++;
+            k++;
+        }
+    }
+}
